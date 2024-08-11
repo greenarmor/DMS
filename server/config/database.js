0af589688ed467a5f9d442fc22c9585e79d6d2c3
greenarmor <greenarmor@live.com>
2024-08-11 20:08:58 +0800

    config creden update

--- server/config/database.js
+++ server/config/database.js
@@ -2,8 +2,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
   "deadmanswitch", // Database name
-  "green", // Database username
-  "w93cqMDo5QaZBnzKdaCPPm2zaQoUqD", // Database password
+  "user", // Database username
+  "**********************", // Database password
   {
     host: "127.0.0.1",
     dialect: "mysql",
